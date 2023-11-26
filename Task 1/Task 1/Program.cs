2f9380dadbd502be8bc418d46a5793a0f8648802
fop1nd <nedly.c63amg@gmail.com>
2023-11-26 21:38:45 +0200

    Fifty

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Address myAddress = new Address();
+
+        myAddress.Index = "12345";
+        myAddress.Country = "Ukraine";
+        myAddress.City = "Kyiv";
+        myAddress.Street = "Revutskogo street";
+        myAddress.House = "10";
+        myAddress.Apartment = "5";
+
+        Console.WriteLine("My Adress:");
+        Console.WriteLine($"Index: {myAddress.Index}");
+        Console.WriteLine($"Country: {myAddress.Country}");
+        Console.WriteLine($"City: {myAddress.City}");
+        Console.WriteLine($"Street: {myAddress.Street}");
+        Console.WriteLine($"House: {myAddress.House}");
+        Console.WriteLine($"Apartament: {myAddress.Apartment}");
+
+        Console.ReadLine();
+    }
+}
