3ad6f7f80325d8725b82690e57db1aa512f099bc
fop1nd <152083647+fop1nd@users.noreply.github.com>
2023-11-27 17:19:05 +0200

    Add files via upload

---
+++ Task 3/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Employee
+{
+    private string lastName;
+    private string firstName;
+
+    public Employee(string lastName, string firstName)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public void CalculateSalaryAndTax(string position, int experience)
+    {
+        double baseSalary = 0;
+        double tax = 0;
+
+        switch (position.ToLower())
+        {
+            case "manager":
+                baseSalary = 30000 + (experience * 1000);
+                break;
+            case "developer":
+                baseSalary = 40000 + (experience * 1500);
+                break;
+            case "analyst":
+                baseSalary = 35000 + (experience * 1200);
+                break;
+            default:
+                Console.WriteLine("Unknown position");
+                break;
+        }
+
+        tax = 0.15 * baseSalary;
+
+        Console.WriteLine($"lastName: {lastName}");
+        Console.WriteLine($"firstName: {firstName}");
+        Console.WriteLine($"position: {position}");
+        Console.WriteLine($"baseSalary: {baseSalary}");
+        Console.WriteLine($"tax: {tax}");
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Employee employee = new Employee("Petrov", "Ivan");
+
+        employee.CalculateSalaryAndTax("developer", 5);
+
+        Console.ReadLine();
+    }
+}
