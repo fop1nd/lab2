2b138d8a0844259bd453b93e81a9dbb53e7082f0
fop1nd <152083647+fop1nd@users.noreply.github.com>
2023-11-27 17:28:28 +0200

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        User user = new User("user123", "Ivan", "Petrov", 30);
+
+        user.DisplayUserInfo();
+
+        Console.ReadLine();
+    }
+}
---
+++ Task4/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class User
+{
+    private string login;
+    private string firstName;
+    private string lastName;
+    private int age;
+    private readonly DateTime registrationDate;
+
+    public User(string login, string firstName, string lastName, int age)
+    {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        registrationDate = DateTime.Now;
+    }
+
+    public void DisplayUserInfo()
+    {
+        Console.WriteLine("Information about the user:");
+        Console.WriteLine($"login: {login}");
+        Console.WriteLine($"firstName: {firstName}");
+        Console.WriteLine($"lastName: {lastName}");
+        Console.WriteLine($"age: {age}");
+        Console.WriteLine($"registrationDate: {registrationDate}");
+    }
+}
