e19c77ca25073f62e0e2651a747b3d4d9c16fafc
fop1nd <152083647+fop1nd@users.noreply.github.com>
2023-11-27 16:51:46 +0200

    Add files via upload

---
+++ Task2/Converter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using System;
+
+class Converter
+{
+    private double usdRate;
+    private double eurRate;
+    private double plnRate;
+
+    public Converter(double usd, double eur, double pln)
+    {
+        usdRate = usd;
+        eurRate = eur;
+        plnRate = pln;
+    }
+
+    public double ConvertToCurrency(double amount, string currency)
+    {
+        double result = 0;
+
+        switch (currency.ToLower())
+        {
+            case "usd":
+                result = amount / usdRate;
+                break;
+            case "eur":
+                result = amount / eurRate;
+                break;
+            case "pln":
+                result = amount / plnRate;
+                break;
+            default:
+                Console.WriteLine("Неправильно введено валюту.");
+                break;
+        }
+
+        return result;
+    }
+
+    public double ConvertFromCurrency(double amount, string currency)
+    {
+        double result = 0;
+
+        switch (currency.ToLower())
+        {
+            case "usd":
+                result = amount * usdRate;
+                break;
+            case "eur":
+                result = amount * eurRate;
+                break;
+            case "pln":
+                result = amount * plnRate;
+                break;
+            default:
+                Console.WriteLine("Неправильно введено валюту.");
+                break;
+        }
+
+        return result;
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Converter converter = new Converter(28.3, 33.2, 7.1);
+
+        double amountInUah = 1000;
+        double convertedToUsd = converter.ConvertToCurrency(amountInUah, "usd");
+        Console.WriteLine($"{amountInUah} UAH = {convertedToUsd} USD");
+
+        double amountInUsd = 50;
+        double convertedToUah = converter.ConvertFromCurrency(amountInUsd, "usd");
+        Console.WriteLine($"{amountInUsd} USD = {convertedToUah} UAH");
+
+        Console.ReadLine();
+    }
+}
